6e1738df82d6e7ddc316bead674d0768ddd73e75
Himanshu3777 <himanshuyadav88813@gmail.com>
2026-07-10 13:11:05 +0530

    Update urls.py

--- website/urls.py
+++ website/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+    path('messages/', views.messages_view, name='messages'),
     path('submit-form/', views.submit_form, name='submit_form'),
 
-    path('messages/', views.messages_view, name='messages'),
 ]
