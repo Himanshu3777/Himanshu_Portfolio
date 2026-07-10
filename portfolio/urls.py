a3ae357dfa42a454e62fd621639a843e12d0b4b4
Himanshu3777 <himanshuyadav88813@gmail.com>
2026-07-10 12:45:37 +0530

    Clean up urls.py by removing extra include paths
    Removed unnecessary import and URL pattern for messages.

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,10 +1,8 @@
-
+# project/urls.py
 from django.contrib import admin
 from django.urls import path, include
-from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('website.urls')),
-    path('messages/', include('your_app.urls')),
+    path('', include('website.urls')),   # मान लिया app का नाम website है
 ]
