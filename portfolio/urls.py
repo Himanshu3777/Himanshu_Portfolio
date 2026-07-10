7025d257ff23f63c6faf266d1bc3aff23dec44e7
Himanshu3777 <himanshuyadav88813@gmail.com>
2026-07-10 12:43:59 +0530

    Update urls.py

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('website.urls'))
+    path('', include('website.urls')),
     path('messages/', include('your_app.urls')),
 ]
